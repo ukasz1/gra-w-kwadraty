05089d842f7fc7a4641fb4f472c1d08130eb1f48
ukasz1 <lukaszmitkowski53@gmail.com>
2019-02-21 20:18:41 +0100

    Square coloring algorithm improved by using modulo function

--- skrypt.js
+++ skrypt.js
@@ -1,34 +1,6 @@
-// jQUERY
-	$(document).ready(function(){
-		
-		if( $("#num5").hasClass('black') ){
-			$('.last-h1').addClass('congratulation');}
-		
-		$('#num5').click(function(){
-			$('#num8').toggleClass('black');
-			$('#num5').toggleClass('black');
-			$('#num6').toggleClass('black');
-		});
-		
-		$('#num6').click(function(){
-			$('#num5').toggleClass('black');
-			$('#num6').toggleClass('black');
-			$('#num7').toggleClass('black');
-		});
-		
-		$('#num7').click(function(){
-			$('#num6').toggleClass('black');
-			$('#num7').toggleClass('black');
-			$('#num8').toggleClass('black');
-		});
-		
-		$('#num8').click(function(){
-			$('#num7').toggleClass('black');
-			$('#num8').toggleClass('black');
-			$('#num5').toggleClass('black');
-		});
-		
-		if(!$("#num5").hasClass('black') ){
-			$('.last-h1').addClass('congratulation');}
-		
-	});
+/*
+
+this is empty...
+...yet
+
+*/
